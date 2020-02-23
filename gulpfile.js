a7dfe851b95550e548f6a523f0744690781b7b55
ivers1 <ivers1984@hotmail.com>
2020-02-23 23:55:30 +0100

    update..

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,6 @@
 const gulp = require("gulp");
 const imagemin = require("gulp-imagemin");
+const browserSync = require("browser-sync").create();
 
 function imgMini(){
     return gulp
@@ -8,12 +9,20 @@ function imgMini(){
     .pipe(gulp.dest("./minified/images"));
 }
 
+gulp.task("browserSync", function(){
+    browserSync.init({
+        server: {
+            baseDir: "ca-workflow2"
+        },
+    })
+})
+
 gulp.task("imgMini", imgMini);
 
 gulp.task("watch", () => {
-    gulp.watch("./img/*", imgMini);
+    gulp.watch("./img/*", imgMini, browserSync);
 });
 
-gulp.task("default", gulp.series("imgMini", "watch")
+gulp.task("default", gulp.series("imgMini", "browserSync", "watch")
 
 );
